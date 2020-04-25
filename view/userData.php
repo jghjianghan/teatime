d01ce7728ddd1a88f273e774b57a11f4f23a2230
vickyricardo <vickyricardo@users.noreply.github.com>
2020-04-25 07:18:50 +0700

    Create userData.php

---
+++ view/userData.php
@@ -0,0 +1,15 @@
+<div id="user-data">
+    <p>Data User</p>
+    <a><button>Add User</button></a>
+    <table>
+        <tr>
+            <th>No</th>
+            <th>Posisi</th>
+            <th>Email</th>
+            <th>Nama Lengkap</th>
+            <th>Tanggal lahir</th>
+            <th>Alamat</th>
+            <th>Aksi</th>
+        </tr>
+    </table>
+</div>
