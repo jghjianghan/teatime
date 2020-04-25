531ae42d21efc32693747972a437b9e491fc0a45
vickyricardo <vickyricardo@users.noreply.github.com>
2020-04-25 07:18:47 +0700

    Create toppingData.php

---
+++ view/toppingData.php
@@ -0,0 +1,12 @@
+<div id="topping-data">
+    <p>Data Topping</p>
+    <a><button>Add Topping</button></a>
+    <table>
+        <tr>
+            <th>No</th>
+            <th>Foto</th>
+            <th>Harga</th>
+            <th>Aksi</th>
+        </tr>
+    </table>
+</div>
