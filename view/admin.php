ebbf9ad1898e7295ac95883a29f837e7af6d843c
vickyricardo <vickyricardo@users.noreply.github.com>
2020-04-25 07:18:40 +0700

    Create admin.php

---
+++ view/admin.php
@@ -0,0 +1,20 @@
+<div id="menu-admin">
+    <p>Pilih Data untuk Dikelola</p>
+    <table id="admin-option">
+        <tr>
+            <th>
+                <img src="https://static.thenounproject.com/png/829929-200.png" width="100" height="100"><br>
+                <a>User</a>
+            </th>
+            <th>
+                <img src="https://static.thenounproject.com/png/228411-200.png" width="120" height="100"><br>
+                <a>Tea</a>
+            </th>
+            <th>
+                <img src="https://static.thenounproject.com/png/1965001-200.png" width="100" height="100"><br>
+                <a>Topping</a>
+            </th>
+        </tr>
+    </table>
+</div>
+
