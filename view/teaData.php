cd0dba439fe513637b4ac4f4fbcf3a9fa25f147b
vickyricardo <vickyricardo@users.noreply.github.com>
2020-04-25 07:18:43 +0700

    Create teaData.php

---
+++ view/teaData.php
@@ -0,0 +1,14 @@
+<div id="tea-data">
+    <p>Data Tea</p>
+    <a><button>Add Tea</button></a>
+    <table>
+        <tr>
+            <th>No</th>
+            <th>Foto</th>
+            <th>Nama</th>
+            <th>Harga Regular</th>
+            <th>Harga Large</th>
+            <th>Aksi</th>
+        </tr>
+    </table>
+</div>
