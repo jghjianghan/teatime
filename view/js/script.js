a0959e0715ac69fe6eb493d600644f6ea6689c74
vickyricardo <vickyricardo@users.noreply.github.com>
2020-04-26 17:34:06 +0700

    update

---
+++ view/js/script.js
@@ -0,0 +1,25 @@
+class pop{
+    constructor(){
+        let btns = document.getElementById('addUser');
+        btns.addEventListener('click', this.showAddUser);
+        
+        btns = document.getElementsByClassName('close');
+        for (let x of btns){
+            x.addEventListener('click', this.closeModal);
+        }
+    }
+
+    showAddUser(){
+        document.getElementById('modal-addUser').style.display = 'block';
+    }
+    showAddTea(){
+        document.getElementById('modal-addTea').style.display = 'block';
+    }
+    showAddTopping(){
+        document.getElementById('modal-addTopping').style.display = 'block';
+    }
+    closeModal(event){
+        event.target.parentNode.parentNode.style.display = 'none';
+    }
+}
+new pop();
--- view/userData.php
+++ view/userData.php
@@ -1,6 +1,6 @@
 <div id="user-data">
     <p>Data User</p>
-    <a href="add-user"><button>Add User</button></a>
+    <button id="addUser">Add User</button><br><br>
     <table class="adminData">
         <tr class="adminData">
             <th class="adminData">No</th>
@@ -42,8 +42,57 @@
         ?>
     </table>
 </div>
+
 <div class="home-button">
     <form method="POST" action="admin">
         <input type="submit" value="Home">
     </form>
+</div>
+
+<div class="modal" id="modal-addUser">
+    <div>
+        <span class='close'>&times;</span>
+        <h2>Add User</h2>
+        <form method="post" action="user/add">
+            <table>
+                <tr>
+                    <td>Posisi</td>
+                    <td>:</td>
+                    <td>
+                        <input type="radio" id="admin" name="posisi" value="admin">
+                        <label for="admin">Admin</label>
+                        <input type="radio" id="manager" name="posisi" value="manager">
+                        <label for="manager">Manager</label>
+                        <input type="radio" id="kasir" name="posisi" value="kasir">
+                        <label for="kasir">Kasir</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="email">Email</label></td>
+                    <td>:</td>
+                    <td><input type="text" id="email" name="email" required></td>
+                </tr>
+                <tr>
+                    <td><label for="nama">Nama Lengkap</label></td>
+                    <td>:</td>
+                    <td><input type="text" id="nama" name="nama" required></td>
+                </tr>
+                <tr>
+                    <td>Tanggal Lahir</td>
+                    <td>:</td>
+                    <td><input type="date" id="ttl" name="ttl" required></td>
+                </tr>
+                <tr>
+                    <td><label for="alamat">Alamat</label></td>
+                    <td>:</td>
+                    <td><textarea id="alamat" rows="4" cols="35" name="alamat" required></textarea></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td><input type="submit" value="Tambahkan"></td>
+                </tr>
+            </table>
+        </form>
+    </div>
 </div>
