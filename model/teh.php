81b38d94fab16ca88c4a329ba3292542259f03ad
Jiang Han <jianghan99@gmail.com>
2020-05-01 20:46:54 +0700

    Moving getAllTea() to Teh Class
    For better reuse. Karena Admin perlu, kasir juga perlu. Manager mungkin saja perlu.

--- model/teh.php
+++ model/teh.php
@@ -25,5 +25,22 @@
         }public function getHargaLarge(){
             return $this->hargaLarge;
         }
+
+        public static function getAllTea()
+        {
+            $db = new MySQLDB("localhost","root","", "teatime");
+            $query="
+                SELECT id, gambar, nama, hargaRegular, hargaLarge
+                FROM Teh
+            ";
+            $query_result = $db->executeSelectQuery($query);
+
+            $result = [];
+            
+            foreach($query_result as $key => $value){
+                $result [] = new Teh($value['id'],$value['gambar'],$value['nama'],$value['hargaRegular'],$value['hargaLarge']);
+            }
+            return $result;
+        }
     }
 ?>
